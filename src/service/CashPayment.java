79ae28ca6f262bf03f1988801c9e3b59eaf0aa0a
InayahKamilaNurman <inayahkamila11@gmail.com>
2025-11-10 10:38:37 +0700

    Implement cash payment processing logic

--- src/service/CashPayment.java
+++ src/service/CashPayment.java
@@ -1,9 +1,32 @@
 package service;
+import java.util.Scanner;
 
 public class CashPayment implements Pembayaran {
 
+    private double total;
+    private boolean status;
+
     @Override
     public void prosesPembayaran(double total) {
-        // TODO: Logika pembayaran cash
+       this.total = total;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n=== PEMBAYARAN TUNAI ===");
+        System.out.println("Total  : Rp" + total);
+        System.out.print("Masukkan jumlah uang tunai: Rp");
+        double uang = input.nextDouble();
+
+        if (uang < total) {
+            System.out.println("❌ Uang tidak cukup, pembayaran dibatalkan.");
+            status = false;
+        } else {
+            double kembalian = uang - total;
+            System.out.println("✅ Pembayaran tunai berhasil!");
+            System.out.println("Kembalian: Rp" + kembalian);
+            status = true;
+        }
+
+        System.out.println("Status transaksi : " + (status ? "Berhasil" : "Gagal"));
+        System.out.println("--------------------------------\n");
     }
 }

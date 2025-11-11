48e2a35ac103df36f9c684d31db7bcbcf228143f
InayahKamilaNurman <inayahkamila11@gmail.com>
2025-11-11 19:04:00 +0700

    Update QRISPayment.java

--- src/service/QRISPayment.java
+++ src/service/QRISPayment.java
@@ -1,9 +1,25 @@
-package service;
+package model;
+
+import java.util.Random;
 
 public class QRISPayment implements Pembayaran {
 
     @Override
-    public void prosesPembayaran(double total) {
-        // TODO: Logika pembayaran QRIS
+    public boolean prosesPembayaran(double total) throws Exception {
+        System.out.println("Silakan scan QRIS untuk pembayaran sebesar Rp" + total);
+        Thread.sleep(2000); // simulasi waktu proses
+        boolean sukses = new Random().nextBoolean();
+
+        if (!sukses) {
+            throw new Exception("Transaksi QRIS gagal. Silakan coba lagi.");
+        }
+
+        System.out.println("Pembayaran QRIS berhasil!");
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Metode Pembayaran: QRIS";
     }
 }
